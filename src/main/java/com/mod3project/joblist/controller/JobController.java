1225b046f112caf203dbe30521e866cc6eeb790c
ml-webdev <74806034+ml-webdev@users.noreply.github.com>
2021-06-25 01:27:35 -0400

    Added a createJob method
    Added a createJob Method to the JobController with @PostMapping("/jobs")

--- src/main/java/com/mod3project/joblist/controller/JobController.java
+++ src/main/java/com/mod3project/joblist/controller/JobController.java
@@ -3,9 +3,7 @@ package com.mod3project.joblist.controller;
 import com.mod3project.joblist.model.Job;
 import com.mod3project.joblist.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,11 @@ public class JobController {
     @GetMapping("/jobs")
     public List<Job> getJob(){
         return this.jobRepository.findAll();
+    }
+
+//    create job REST api
+    @PostMapping("/jobs")
+    public Job createJob(@RequestBody Job job){
+        return jobRepository.save(job);
     }
 }
