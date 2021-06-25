d453b4dfb433f1f7df9e2a7978aea9ab2ef8c444
ml-webdev <74806034+ml-webdev@users.noreply.github.com>
2021-06-25 01:21:30 -0400

    Configured Routing
    Configured Routing, added Header and Footer Components

--- joblist/src/App.js
+++ joblist/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import JobListComponent from './components/JobListComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <JobListComponent />
+    <div>
+      <Router>
+        <HeaderComponent /> 
+        <div className="container">
+          <Switch> 
+            <Route path = "/" component = {JobListComponent}></Route>
+            <Route path = "/jobs" component = {JobListComponent}></Route>
+            <JobListComponent />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
